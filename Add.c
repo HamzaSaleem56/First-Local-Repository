0964488658d733009e12c7613803f10244d236e2
Hamza Saleem <saleem4600460@cloud.neduet.edu.pk>
2024-01-01 16:33:02 +0500

    First commit in Local Repository

---
+++ Add.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int a,b,c=0;
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    c = a + b;
+    printf("The sum of two numbers using input from user: %d",c);
+}
